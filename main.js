5317d4fe25249099faafd6f1344fda19485b6506
dblinkhorn <dblinkhorn@gmail.com>
2021-09-12 19:00:26 -0700

    got data appending to DOM and some styling. need to fix issue of clearing previous search before appending new search results.

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
-const api_key = '5053bf0831a345a79eb1d207b066c9f1'
+const api_key = '5053bf0831a345a79eb1d207b066c9f1';
+const submit = document.getElementById('submit');
+const stats = document.getElementsByClassName('stat');
+const statsContainer = document.getElementsByClassName('stat-container');
+
+submit.addEventListener('click', (event) => {
+  if (stats.length > 0) {
+    for (let stat = 1; stat < stats.length; stat++) {
+      statsContainer.removeChild(stats);
+    }
+  }
+  event.preventDefault();
+})
+
+// get user search input and initiate function chain to display weather
+function getSearch() {
+  const search = document.getElementById('search');
+  const userSearch = search.value;
+  getCityCoords(userSearch);
+}
 
 // get city coordinates from api call
 async function getCityCoords(city) {
@@ -11,16 +30,14 @@ async function getCityCoords(city) {
     const latitude = data.city.coord.lat;
     const longitude = data.city.coord.lon;
     const coords = { latitude, longitude };
-    return coords;
+    getWeather(coords);
   } catch (error) {
     console.log(error);
   }
 }
 
-const coords = getCityCoords('london');
-
 // get weather data from city coordinates
-async function getWeather(coords) {
+async function getWeather(coords, units) {
   try {
     const cityCoords = await coords;
     const response = await fetch(
@@ -28,13 +45,13 @@ async function getWeather(coords) {
       `&lon=${cityCoords.longitude}&exclude=minutely,hourly&units=imperial&appid=${api_key}`
       );
     const data = await response.json();
-    return data;
+    processCurrentWeather(data);
+    processDailyWeather(data);
+    // return data;
   } catch (error) {
     console.log(error);
   }
 }
-
-const data = getWeather(coords);
 
 // capitalize first letter of each word in condition
 function caseCondition(condition) {
@@ -43,68 +60,140 @@ function caseCondition(condition) {
   return casedCondition;
 }
 
-// process weather data into separate variables and for DOM
-async function processWeather(data) {
+// processes current weather stats into an object/properties
+async function processCurrentWeather(data) {
+  try {
+    const weather = await data;
 
-  const weather = await data;
-
-  const currentWeather = {
-    currentTemp: weather.current.temp,
-    currentCondition: caseCondition(weather.current.weather[0].description),
-    currentCloudCover: weather.current.clouds,
-    currentWindSpeed: weather.current.wind_speed,
-    currentHumidity: weather.current.humidity,
-    currentUVIndex: weather.current.uvi
-  }
-
-  console.log('Current Temperature: ' + currentWeather.currentTemp + 'F');
-  console.log('Condition: ' + currentWeather.currentCondition);
-  console.log('Cloud Cover: ' + currentWeather.currentCloudCover + '%');
-  console.log('Wind Speed (mph): ' + currentWeather.currentWindSpeed);
-  console.log('Humidity: ' + currentWeather.currentHumidity + '%');
-  console.log('UV Index: ' + currentWeather.currentUVIndex);
-
-  for (let i = 1; i < weather.daily.length; i++) {
-    const unixTimestamp =  weather.daily[i].dt;
-    const dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    const date = new Date(unixTimestamp*1000).toLocaleDateString("en-us", dateOptions);
-
-    const dailyWeather = {
-      dailyHigh: weather.daily[i].temp.max,
-      dailyLow: weather.daily[i].temp.min,
-      dailyCondition: caseCondition(weather.daily[i].weather[0].description),
-      dailyWindSpeed: weather.daily[i].wind_speed,
-      dailyHumidity: weather.daily[i].humidity,
-      dailyUVIndex: weather.daily[i].uvi
+    const currentWeather = {
+      currentTemp: weather.current.temp,
+      currentCondition: caseCondition(weather.current.weather[0].description),
+      currentCloudCover: weather.current.clouds,
+      currentWindSpeed: weather.current.wind_speed,
+      currentHumidity: weather.current.humidity,
+      currentUVIndex: weather.current.uvi
     }
+  
+    console.log('Current Temperature: ' + currentWeather.currentTemp + 'F');
+    console.log('Condition: ' + currentWeather.currentCondition);
+    console.log('Cloud Cover: ' + currentWeather.currentCloudCover + '%');
+    console.log('Wind Speed (mph): ' + currentWeather.currentWindSpeed);
+    console.log('Humidity: ' + currentWeather.currentHumidity + '%');
+    console.log('UV Index: ' + currentWeather.currentUVIndex);
 
-    console.log(date)
-    console.log('High Temperature: ' + dailyWeather.dailyHigh);
-    console.log('Low Temperature: ' + dailyWeather.dailyLow);
-    console.log('Condition: ' + dailyWeather.dailyCondition);
-    console.log('Wind Speed (mph): ' + dailyWeather.dailyWindSpeed);
-    console.log('Humidity: ' + dailyWeather.dailyHumidity + '%');
-    console.log('UV Index: ' + dailyWeather.dailyUVIndex);
+    return currentWeather;
+
+  } catch (error) {
+    console.log(error);
   }
 }
+
+// processes daily weather stats into an object/properties
+async function processDailyWeather(data) {
+  try {
+    const weather = await data;
+    const dailyList = [];
+
+    for (let i = 1; i < weather.daily.length; i++) {
+      const unixTimestamp =  weather.daily[i].dt;
+      const dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+      const date = new Date(unixTimestamp*1000).toLocaleDateString("en-us", dateOptions);
+  
+      const dailyWeather = {
+        dailyDate: date,
+        dailyHigh: weather.daily[i].temp.max,
+        dailyLow: weather.daily[i].temp.min,
+        dailyCondition: caseCondition(weather.daily[i].weather[0].description),
+        dailyWindSpeed: weather.daily[i].wind_speed,
+        dailyHumidity: weather.daily[i].humidity,
+        dailyUVIndex: weather.daily[i].uvi
+      }
+      dailyList.push(dailyWeather);
+    }
+    appendDailyToDOM(dailyList);
+  } catch(error) {
+    console.log(error);
+  }
+}
+
+const dailyCategoriesDiv = document.getElementById('daily-categories');
+const dailyDateDiv = document.getElementById('daily-date');
+const dailyHighTempsDiv = document.getElementById('high-temps');
+const dailyLowTempsDiv = document.getElementById('low-temps');
+const dailyConditionDiv = document.getElementById('daily-condition');
+const dailyWindSpeedDiv = document.getElementById('daily-wind-speed');
+const dailyHumidityDiv = document.getElementById('daily-humidity');
+const dailyUVIndexDiv = document.getElementById('daily-uv-index');
+
+// appends data from daily process function to DOM
+async function appendDailyToDOM(weatherData) {
+  const dailyData = await weatherData;
+
+  dailyData.forEach(day => {
+
+    const dailyDate = document.createElement('div');
+    dailyDate.id = 'daily-date';
+    dailyDate.classList = 'stat';
+    dailyDate.textContent = day.dailyDate;
+    dailyDateDiv.appendChild(dailyDate);
+
+    const dailyHighTemp = document.createElement('div');
+    dailyHighTemp.id = 'high-temp';
+    dailyHighTemp.classList = 'stat';
+    dailyHighTemp.textContent = day.dailyHigh + '°';
+    dailyHighTempsDiv.appendChild(dailyHighTemp);
+
+    const dailyLowTemp = document.createElement('div');
+    dailyLowTemp.id = 'low-temp';
+    dailyLowTemp.classList = 'stat';
+    dailyLowTemp.textContent = day.dailyLow + '°';
+    dailyLowTempsDiv.appendChild(dailyLowTemp);
+
+    const dailyCondition = document.createElement('div');
+    dailyCondition.id = 'daily-condition';
+    dailyCondition.classList = 'stat';
+    dailyCondition.textContent = day.dailyCondition;
+    dailyConditionDiv.appendChild(dailyCondition);
+
+    const dailyWindSpeed = document.createElement('div');
+    dailyWindSpeed.id = 'daily-wind-speed';
+    dailyWindSpeed.classList = 'stat';
+    dailyWindSpeed.textContent = day.dailyWindSpeed;
+    dailyWindSpeedDiv.appendChild(dailyWindSpeed);
+
+    const dailyHumidity = document.createElement('div');
+    dailyHumidity.id = 'daily-humidity';
+    dailyHumidity.classList = 'stat';
+    dailyHumidity.textContent = day.dailyHumidity + '%';
+    dailyHumidityDiv.appendChild(dailyHumidity);
+
+    const dailyUVIndex = document.createElement('div');
+    dailyUVIndex.id = 'daily-uv-index';
+    dailyUVIndex.classList = 'stat';
+    dailyUVIndex.textContent = day.dailyUVIndex;
+    dailyUVIndexDiv.appendChild(dailyUVIndex);
+  })
+}
+
+// appendToDOM(dailyList);
+
+// const dw = dailyWeather;
+// const cw = currentWeather;
 
 // async function convertTime(data) {
 //   const time_data = await data;
 //   let unix_timestamp = time_data;
-//   // Create a new JavaScript Date object based on the timestamp
-//   // multiplied by 1000 so that the argument is in milliseconds, not seconds.
 //   let date = new Date(unix_timestamp * 1000);
-//   // Hours part from the timestamp
 //   let hours = date.getHours();
-//   // Minutes part from the timestamp
 //   let minutes = "0" + date.getMinutes();
-//   // Seconds part from the timestamp
 //   let seconds = "0" + date.getSeconds();
 
-//   // Will display time in 10:30:23 format
+//   // will display time in hh:mm:ss format
 //   let formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
 
 //   return formattedTime;
 // }
 
-processWeather(data);
+// processCurrentWeather(data);
+// appendDailyToDOM(dailyList);
+// processDailyWeather(data);
